da42075dd3af2d03fa816b32b2465d7165227758
Marcin Jenczmyk <mjenczmyk@us.edu.pl>
2019-03-06 15:41:45 +0100

    PyPi configuration

--- setup.py
+++ setup.py
@@ -20,6 +20,11 @@ dev_requirements = [
 setup(
     name='packt',
     version=package_version,
+    license='MIT',
+    description='Script for grabbing daily Packt Free Learning ebooks',
+    author='Łukasz Uszko',
+    author_email='lukasz.uszko@gmail.com',
+    url='https://github.com/luk6xff/Packt-Publishing-Free-Learning',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     py_modules=['packtPublishingFreeEbook', 'api'],
@@ -29,5 +34,6 @@ setup(
         'console_scripts': [
             'packt-cli = packtPublishingFreeEbook:packt_cli',
         ],
-    }
+    },
+    download_url='https://github.com/luk6xff/Packt-Publishing-Free-Learning/archive/v1.0.2.tar.gz'
 )
