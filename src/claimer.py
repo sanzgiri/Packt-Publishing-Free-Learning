a050080e7b885b9bc3eb6bdce9af68638ad32a03
laurentlemercier <22855554+laurentlemercier@users.noreply.github.com>
2020-05-20 23:19:03 +0200

    Handle case when there is no Free Learning offer #176 (#189)
    Co-authored-by: laurent.lemercier <pi@raspberrypi>

--- src/claimer.py
+++ src/claimer.py
@@ -59,6 +59,12 @@ def claim_product(api_client, recaptcha_solution):
             'dateTo': (utc_today + dt.timedelta(days=1)).isoformat()
         }
     )
+    # Handle case when there is no Free Learning offer
+    offer_count = offer_response.json().get('count')
+    if offer_count == 0:
+        logger.info("There is no Free Learning offer right now")
+        raise Exception("There is no Free Learning offer right now")
+
     [offer_data] = offer_response.json().get('data')
     offer_id = offer_data.get('id')
     product_id = offer_data.get('productId')
